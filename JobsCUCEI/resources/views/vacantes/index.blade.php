f1c32dafb97f040333aedf092381a588041203a1
Jose Rodriguez <jose.rodriguez2962@alumnos.udg.mx>
2023-03-31 01:25:24 -0600

    Creacion de migraciones, insercion de vacantes, creacion de validaciones y formulario para insertar vacante

---
+++ JobsCUCEI/resources/views/vacantes/index.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Mis vacantes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+           @if(session()->has('mensaje'))
+           <div class="uppercase border border-green-600 bg-green-100 text-green-600 font-bold p-2 my-3">
+            {{session('mensaje')}}
+
+           </div>
+            
+           @endif
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Mis vacantes") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
